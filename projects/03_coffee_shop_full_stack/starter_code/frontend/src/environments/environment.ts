d00b2ced517656af16e0ee8cfbe76370fb35a2a5
aaronmclean1 <am9518@att.com>
2019-12-08 22:21:07 -0600

    fixed env URL var and removed token var at the test level

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:57959', // the running FLASK api server url
   auth0: {
-    url: 'acm123.auth0.com', // the auth0 domain prefix
+    url: 'acm123', // the auth0 domain prefix
     audience: 'image', // the audience set for the auth0 app
     clientId: '1L9xw5s0XGD5po82oL6LoGJxLOKNYJtP', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:57918', // the base url of the running ionic app. Must put http://localhost:57064/tabs/user-page in auth0 website
